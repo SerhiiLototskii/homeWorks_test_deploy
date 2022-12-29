c2a2f270b772d0fc557cc0522fef4d2a08faa02e
SerhiiLototskyi <lototskiysergey@gmail.com>
2022-12-29 18:49:32 +0200

    changed task 4

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -32,6 +32,9 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
         if(onChangeChecked) {
             onChangeChecked(e.currentTarget.checked)
         }
+        if (onChange) {
+            onChange(e)
+        }
 
     }
 
