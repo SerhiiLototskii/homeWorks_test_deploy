506f5384cf91e8a574db6b0d06c8f6c1a729c190
SerhiiLototskyi <lototskiysergey@gmail.com>
2023-01-20 04:31:48 +0200

    completed hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,26 @@
 const initState = {
     themeId: 1,
 }
+type initialStateType = {
+    themeId: number
+}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: changeThemeAC): initialStateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":{
+            let stateCopy = {...state}
+            stateCopy.themeId = action.id
+            return stateCopy
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): changeThemeAC => ({ type: 'SET_THEME_ID', id }) // fix any
+
+type changeThemeAC = {
+    id:number
+    type: 'SET_THEME_ID'
+}
